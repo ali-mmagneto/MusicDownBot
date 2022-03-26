1e53eb0b4b1c15d6fabe1e6912fc00d86fbdd4bb
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-03-26 14:48:53 +0300

    Update and rename config (1).py to config.py

--- config.py
+++ config.py
@@ -4,7 +4,5 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    BOT_OWNER = os.environ.get("BOT_OWNER")
+    SUDO = os.environ.get("SUDO")
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
